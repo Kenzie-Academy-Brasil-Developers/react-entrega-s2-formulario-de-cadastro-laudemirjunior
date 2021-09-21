842b9369b693cb2fd3a7bf5d4e935c33ff63f7d0
Laudemir <nlaudemir@gmail.com>
2021-09-21 17:35:42 -0300

    entrega de atividade

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -4,15 +4,16 @@ import Form from "../pages/Form/Form";
 import { useState } from "react";
 
 const Routers = () => {
+  const [obj, setObj] = useState([]);
   const [logado, setLogado] = useState(false);
   return (
     <div>
       <Switch>
         <Route exact path="/">
-          <Form setLogado={setLogado} />
+          <Form setLogado={setLogado} setObj={setObj} />
         </Route>
-        <Route path="/home" logado={logado}>
-          <Home from />
+        <Route path="/home">
+          <Home from logado={logado} obj={obj} />
         </Route>
       </Switch>
     </div>
